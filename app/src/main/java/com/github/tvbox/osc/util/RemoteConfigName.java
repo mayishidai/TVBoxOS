3d539a98f47b5930330040333edf40af4f585c32
jyt0106 <107200165+jyt0106@users.noreply.github.com>
2022-09-16 16:01:33 +0800

    Create RemoteConfigName.java

---
+++ app/src/main/java/com/github/tvbox/osc/util/RemoteConfigName.java
@@ -0,0 +1,9 @@
+package com.github.tvbox.osc.util;
+
+// 远程配置文件名
+public class RemoteConfigName {
+    public static final String IsRecodeLog="IsRecodeLog"; // 是否开启日志记录
+    public static final String APIUrl="APIUrl"; // 默认首页API地址
+    public static final String UpdateUrl="UpdateUrl"; // 默认更新地址
+    public static final String IsForceUpdate="IsForceUpdate"; // 是否强制显示更新
+}
