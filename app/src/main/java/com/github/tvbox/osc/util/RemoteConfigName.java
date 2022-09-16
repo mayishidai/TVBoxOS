ea82b4c75d8c99714a85c109e9d4725e8b107822
jyt0106 <107200165+jyt0106@users.noreply.github.com>
2022-09-16 20:14:30 +0800

    Update RemoteConfigName.java

--- app/src/main/java/com/github/tvbox/osc/util/RemoteConfigName.java
+++ app/src/main/java/com/github/tvbox/osc/util/RemoteConfigName.java
@@ -4,6 +4,10 @@ package com.github.tvbox.osc.util;
 public class RemoteConfigName {
     public static final String IsRecodeLog="IsRecodeLog"; // 是否开启日志记录
     public static final String APIUrl="APIUrl"; // 默认首页API地址
-    public static final String UpdateUrl="UpdateUrl"; // 默认更新地址
-    public static final String IsForceUpdate="IsForceUpdate"; // 是否强制显示更新
+      public static final String IsForceUpdate="IsForceUpdate"; // 是否首页强制显示更新
+    public static final String UpdateData="UpdateData"; // 更新数据
+    public static final String UpdateData_NewVersion="NewVersion"; // 更新数据_新版本号
+    public static final String UpdateData_ForceUpdate="ForceUpdate"; // 更新数据_是否强制更新
+    public static final String UpdateData_UpdateDesc="UpdateDesc"; // 更新数据_更新描述
+    public static final String UpdateData_UpdateDownloadUrl="UpdateDownloadUrl"; // 更新数据_更新下载地址
 }
