1f91a55ca0f0edd1ceaad89b25f14ff80fa7563c
mayishidai <1620829248@qq.com>
2022-11-05 17:45:21 +0800

    logcat 采集保存

--- app/src/main/java/com/github/tvbox/osc/util/LOG.java
+++ app/src/main/java/com/github/tvbox/osc/util/LOG.java
@@ -7,6 +7,9 @@ import com.github.tvbox.osc.base.App;
 import com.tencent.bugly.crashreport.CrashReport;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -20,6 +23,7 @@ public class LOG {
     private static boolean isSaveLog = false;
     private static int saveDay = 2;
     private static File file;
+    private static File logcatFile;
 
     public static void i(Object... msgs){
         LogPrint(Log.INFO, FormatMsg(msgs));
@@ -33,13 +37,15 @@ public class LOG {
     public static void e(Object... msgs){
         LogPrint(Log.ERROR, FormatMsg(msgs));
     }
-    public static void printStackTrace(Exception ex){
+    public static void printStackTrace(Exception ex, Object... msgs){
         e(ex);
+        e(msgs);
         ex.printStackTrace();
         CrashReport.postCatchedException(ex);
     }
-    public static void printStackTrace(Throwable th){
+    public static void printStackTrace(Throwable th, Object... msgs){
         e(th);
+        e(msgs);
         th.printStackTrace();
         CrashReport.postCatchedException(th);
     }
@@ -59,7 +65,7 @@ public class LOG {
                     for (int i = 0; i < tempList.length; i++) {
                         if (tempList[i].isFile()) {
                             String fileName = tempList[i].getName();
-                            if (fileName.endsWith(".log")){    //  根据自己的需要进行类型筛选
+                            if (fileName.endsWith(".log") || fileName.endsWith(".logcat")){    //  根据自己的需要进行类型筛选
                                 try {
                                     //文件时间减去当前时间
                                     Date start = dateFormat.parse(dateFormat.format(new Date(tempList[i].lastModified())));
@@ -78,12 +84,19 @@ public class LOG {
                 file = new File(logDir, dateFormat.format(now)+".log");
                 if (!file.exists())
                     file.createNewFile();
-                e("日志文件存储位置：", file.getAbsolutePath());
+                d("普通日志文件存储位置：", file.getAbsolutePath());
+
+                logcatFile = new File(logDir, dateFormat.format(now)+".logcat");
+                if (!logcatFile.exists())
+                    logcatFile.createNewFile();
+                OpenLogcat();
+                d("Logcat日志文件存储位置：", logcatFile.getAbsolutePath());
             }
         }catch (Exception e){
             e.printStackTrace();
             isSaveLog = false;
             file = null;
+            logcatFile = null;
         }
     }
     public static void ClsoeSaveLog(){
@@ -116,10 +129,50 @@ public class LOG {
         if (isSaveLog && file!=null)
             FileUtils.appendFile(file, String.format("%s   %s\n", logType, msg));
     }
-
     public static byte[] ReadLogFile(){
         if (isSaveLog && file!=null)
             return FileUtils.readSimple(file);
         return null;
+    }
+
+    public static void OpenLogcat(){
+        //第一个是Logcat ，也就是我们想要获取的log日志
+        //第二个是 -s 也就是表示过滤的意思
+        //第三个就是 我们要过滤的类型 W表示warm ，我们也可以换成 D ：debug， I：info，E：error等等
+        String[] running = new String[]{"logcat","-s","adb logcat *: D"};
+        try {
+            Process exec = Runtime.getRuntime().exec(running);
+            final InputStream inputS = exec.getInputStream();
+            new Thread() {
+                @Override
+                public void run() {
+                    FileOutputStream os = null;
+                    try {
+                        Log.e(TAG,"开始在线写入logcat");
+                        //新建一个路径信息
+                        os = new FileOutputStream(logcatFile);
+                        int len = 0;
+                        byte[] buf = new byte[1024];
+                        while (-1 != (len = inputS.read(buf))) {
+                            os.write(buf, 0, len);
+                            os.flush();
+                        }
+                    } catch (Exception e) {
+                        printStackTrace(e, "读取Logcat线程报错");
+                    } finally {
+                        if (null != os) {
+                            try {
+                                os.close();
+                                os = null;
+                            } catch (IOException e) {
+                                // Do nothing
+                            }
+                        }
+                    }
+                }
+            }.start();
+        } catch (Exception e) {
+            printStackTrace(e, "打开Logcat线程报错");
+        }
     }
 }
